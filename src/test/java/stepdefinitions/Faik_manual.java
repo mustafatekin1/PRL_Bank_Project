98d7c8025b725df65679c89337ff216f1370fe9e
FaikAliEkremoglu <sebiyelda21122020@gmail.com>
2021-11-17 21:27:07 +0300

    changes

--- src/test/java/stepdefinitions/Faik_manual.java
+++ src/test/java/stepdefinitions/Faik_manual.java
@@ -3,6 +3,6 @@ package stepdefinitions;
 public class Faik_manual {
 
     public void bugFinderLastProject(){
-        System.out.println("Allah yardımcımız olsun!");
+        System.out.println("Allah hepimizin yardımcısı olsun!");
     }
 }
