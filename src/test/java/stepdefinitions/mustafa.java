93e89e209afcceb4163cdfd7ab7504989e6dfab9
mustafasdet <mustafa.sdet@gmail.com>
2021-11-18 23:13:57 +0300

    commit upstream

--- src/test/java/stepdefinitions/mustafa.java
+++ src/test/java/stepdefinitions/mustafa.java
@@ -3,6 +3,10 @@ package stepdefinitions;
 public class mustafa {
     public static void main(String[] args) {
         System.out.println("first connection");
+
+        System.out.println("push upstream is required?");
+
     }
+
 
 }
