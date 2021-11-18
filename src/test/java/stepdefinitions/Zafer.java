590a867fd79abc7fc79ee076c40aec208afd1a54
zafer çakar <tarkankanat777@gmail.com>
2021-11-18 22:26:30 +0300

    ilk lokal

--- src/test/java/stepdefinitions/Zafer.java
+++ src/test/java/stepdefinitions/Zafer.java
@@ -3,6 +3,7 @@ package stepdefinitions;
 public class Zafer {
     public void zafer(){
         System.out.println("tarkan");
-
+        System.out.println("zafer");
+        System.out.println("kırmızı");
     }
 }
