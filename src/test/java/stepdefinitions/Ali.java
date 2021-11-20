342d3a6e6ee7e9ae26516d3b16c80c73d5f2f5ff
Emel614 <emelny614@gmail.com>
2021-11-21 00:11:38 +0100

    new

--- src/test/java/stepdefinitions/Ali.java
+++
@@ -1,7 +0,0 @@
-package stepdefinitions;
-
-public class Ali {
-    public void setup(){
-      
-        }
-}
--- src/test/java/stepdefinitions/Sadik.java
+++
@@ -1,7 +0,0 @@
-package stepdefinitions;
-
-public class Sadik {
-    public static void main(String[] args) {
-        System.out.println("bsml");
-    }
-}
