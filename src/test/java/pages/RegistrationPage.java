e636558ac078089349eb727eca141fb57518c2e9
ali <comdarici@gmail.com>
2021-11-21 11:36:04 +0100

    lastname

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -9,10 +9,13 @@ public class RegistrationPage {
     public RegistrationPage(){
         PageFactory.initElements(Driver.getDriver(),this); }
 
+    @FindBy(id = "ssn")
+    public WebElement ssn;
 
+    @FindBy(xpath= "(//input[@type='text'])[2]")
+    public WebElement firstName;
 
-
-
-
+    @FindBy(xpath = "(//input[@type='text'])[3]")
+    public WebElement lastName;
 
 }
