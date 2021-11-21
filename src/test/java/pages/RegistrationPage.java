6226ea7b1b32aeb7c2c5b9c0e28da8dcf1ccd09c
bhrtrk <bahar.dk@gmail.com>
2021-11-21 12:23:12 +0100

    update

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -7,29 +7,27 @@ import utilities.Driver;
 
 public class RegistrationPage {
     public RegistrationPage(){
-<<<<<<< HEAD
+
         PageFactory.initElements(Driver.getDriver(),this);
 
     }
-=======
-        PageFactory.initElements(Driver.getDriver(),this);}
->>>>>>> master
-
 
     @FindBy(id = "ssn")
     public WebElement ssn;
 
     @FindBy(id="email")
     public WebElement email;
+
     @FindBy(id="firstPassword")
     public WebElement firstPassword;
+
     @FindBy(id="secondPassword")
     public WebElement secondPassword;
 
     @FindBy(id="login-item")
     public WebElement signInButton;
   
-   @FindBy(xpath="(//a[@class='dropdown-item'])[2]")
+    @FindBy(xpath="(//a[@class='dropdown-item'])[2]")
     public WebElement registerButton;
 
     @FindBy(xpath= "(//input[@type='text'])[2]")
